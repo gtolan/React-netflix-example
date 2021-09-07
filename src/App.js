73d76e0254fac15abf38b48bf178637e9cb42d27
GTolan <tolangerard@gmail.com>
2021-09-07 12:32:47 +0100

    feat: touch swipe component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import SwipeSlider from './components/SwipeSlider';
+
 
 function App() {
   return (
@@ -9,14 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <SwipeSlider />
       </header>
     </div>
   );
